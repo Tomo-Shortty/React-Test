66431d404f0d40562e24a637faaf26a6266f7458
Tomo-Shortty <tomo_007@live.com.au>
2021-02-10 18:41:23 +1000

    Initial Project Commit
    Commit to move initial project files to the repository.

---
+++ Grocery List Builder/Grocery List Builder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Grocery_List_Builder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Grocery List Builder/Grocery List Builder/wwwroot/js/GroceryList.jsx
@@ -0,0 +1,9 @@
+﻿class GroceryList extends React.Component {
+    render() {
+        return (
+            <div className="groceryList">Grocery List to go here.</div>
+        );
+    }
+}
+
+ReactDOM.render(<GroceryList />, document.getElementById('content'));
